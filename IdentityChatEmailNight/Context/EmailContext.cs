93d19932a930cf306c5ffd7e7d583dcb3c863ef1
Kadir-Hali <kadirhali07@gmail.com>
2025-04-29 20:54:26 +0300

    Add project files.

---
+++ IdentityChatEmailNight/Context/EmailContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityChatEmailNight.Context
+{
+    public class EmailContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS; Initial Catalog=EmailChatNightDb; Integrated Security=true; Trust Server Certificate=true");
+        }
+    }
+}
