1a7dc76ab0670ed64b8b52390dd5e77ba902f162
Kadir-Hali <kadirhali07@gmail.com>
2025-05-01 22:08:51 +0300

    01.05.25

---
+++ IdentityChatEmailNight/Models/CustomIdentityValidator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityChatEmailNight.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Lütfen en az bir tane büyük harf girişi yapınız!"
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Lütfen en az bir tane küçük harf girişi yapınız!"
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Lütfen en az bir tane rakam girişi yapınız!"
+            };
+        }
+
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphaNumeric",
+                Description = "Lütfen en az bir tane sembol girişi yapınız!"
+            };
+        }
+
+        public override IdentityError PasswordTooShort(int lenght)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Lütfen en az {lenght} karakter uzunluğunda şifre girişi yapınız!"
+            };
+        }
+    }
+}
